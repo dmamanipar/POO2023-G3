2bf5738c4e4e5cf875f9fafebeb39ff3012c2be6
dmamanipar <dmamanipar@unsa.edu.pe>
2023-03-29 15:35:18 +0000

    Aplicando el concepto de herencia

---
+++ herencia/Vehiculo.java
@@ -0,0 +1,9 @@
+public class Vehiculo { //Clase Padre
+    public String marca="Toyota";
+    protected String color;
+    protected String modelo;
+
+    public String sonido() {
+       return "Tuut tuut...";
+    }
+}
