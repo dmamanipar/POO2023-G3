8522bfdfe19c9f28f7da54e55f254e1b1ecaf111
dmamanipar <dmamanipar@unsa.edu.pe>
2023-03-29 14:54:08 +0000

    Aplicacndo concepto de Objeto

--- claseobjetoencapsulamiento/ClaseGeneral.java
+++ claseobjetoencapsulamiento/ClaseGeneral.java
@@ -1,8 +1,13 @@
 public class ClaseGeneral {
 
     public static void main(String[] args) {
-        Persona.nombre="Dario";
+       /* Persona.nombre="Dario";
         Persona.edad=20;
-        Persona.saludo();
+        Persona.saludo();*/
+
+        Persona p1Obj=new Persona(); //Concepto de Objetos
+        p1Obj.nombre="Raul";
+        p1Obj.edad=21;
+        p1Obj.saludo();
     }
 }
--- claseobjetoencapsulamiento/Persona.java
+++ claseobjetoencapsulamiento/Persona.java
@@ -1,9 +1,9 @@
 
 public class Persona {
-    public static  String nombre;
-    public static int edad;
+    public   String nombre;
+    public  int edad;
     
-    public static void saludo() {
+    public  void saludo() {
         System.out.println("Hola, soy "+nombre+" y mi edad es:"+edad);
     }
 }
