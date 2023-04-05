117eca71ec1cec24ba317f6cad6f2a37cf6fe0ae
dmamanipar <dmamanipar@unsa.edu.pe>
2023-04-05 08:57:54 -0500

    clase interna

---
+++ claseinterna/Operacion.java
@@ -0,0 +1,27 @@
+package claseinterna;
+
+import claseinterna.Operacion.OperMatematicos;
+
+public class Operacion {
+    int a;
+    int b;
+    public int suma() {
+        return a+b;
+    }
+    public class OperMatematicos {
+    int resultados;
+
+    public double potencia(int num, int nump){
+        return Math.pow(num, nump);
+    }        
+    }
+}
+
+ class Principal {
+public static void main(String[] args) {
+    Operacion o=new Operacion();
+    OperMatematicos om=o.new OperMatematicos();
+    System.out.println(om.potencia(2, 3));
+}    
+
+}
