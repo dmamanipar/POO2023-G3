382e6f1219629d562159ab36b6d1eac82c59dd42
Jhackns <103521582+Jhackns@users.noreply.github.com>
2023-05-12 06:32:16 -0500

    elim

--- encapsulamientointerface/Animal.java
+++
@@ -1,6 +0,0 @@
-
-
-public interface Animal {    
-    public void emitirSonido();
-    public void dormir();    
-}
--- encapsulamientointerface/Principal.java
+++
@@ -1,8 +0,0 @@
-public class Principal {
-    
-    public static void main(String[] args) {
-        Animal objA=new Loro();
-        objA.emitirSonido();
-        objA.dormir();
-    }
-}
