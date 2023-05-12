a2a7bc3a225ce670daad60ba1d5bce07d387f4b8
Jhackns <103521582+Jhackns@users.noreply.github.com>
2023-05-12 06:32:42 -0500

    elim

--- herencia/Vehiculo.java
+++
@@ -1,9 +0,0 @@
-public class Vehiculo { //Clase Padre
-    public String marca="Toyota";
-    protected String color;
-    protected String modelo;
-
-    public String sonido() {
-       return "Tuut tuut...";
-    }
-}
